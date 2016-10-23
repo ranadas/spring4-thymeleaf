f54592dd984db9d452e3b1d5545f04990c16ff86
Rana Das <rana.pratap.das@gmail.com>
2016-10-23 10:18:25 +0100

    adding gul util logging

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,34 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util');
 
 //Ignore all .min files
 gulp.task('vet', function () {
-    console.log('\n--> Vetting all js files\n');
+    log('\n--> Vetting, Analyzing js files with jscs and Jshint.\n');
     return gulp
         .src(['./src/**/*.js',
             './*.js',
             '!./src/**/*.min.js'])
         .pipe(jscs())
         .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose:true}));
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
 });
 
 
 gulp.task('hello-world', function () {
     console.log('Hello Rana');
 });
+
+function log(message) {
+    if (typeof (message) === 'object') {
+        for (var item in message) {
+            if (message.hasOwnProperty(item)) {
+                util.log(util.colors.blue(message[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(message));
+    }
+}
 
